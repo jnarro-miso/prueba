632ed22191bbd29eb5cbad17da80e724d8a78748
Zamir Narro <josue.narro.a@gmail.com>
2024-03-07 20:46:45 -0500

    Agrega prueba

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -3,5 +3,13 @@ from Comunidad.persona import Persona
 
 class PersonaTestCase(unittest.TestCase):
 
-    def test_prueba(self):
-        self.assertEqual(0, 0)
+    def setUp(self):
+        self.persona1 = Persona(nombre='Alejandra', edad=25)
+        self.persona2 = Persona(nombre='Diego', edad=22)
+        self.persona3 = Persona(nombre='Alejandra', edad=25)
+        self.persona4 = Persona(nombre='Diana', edad=25)
+        self.grupo = [self.persona1, self.persona2, self.persona3]
+
+    def test_constructor(self):
+        self.assertEqual(self.persona1.dar_nombre(), 'Alejandra')
+        self.assertEqual(self.persona1.dar_edad(), 25)
